a430a8df55841dd4487c517dd9908947e8a10bee
AwesomeAdam1 <adamwang.aw.2006@gmail.com>
2026-06-28 21:55:57 -0400

    Added textures

--- hittable.h
+++ hittable.h
@@ -2,6 +2,7 @@
 #define HITTABLE_H
 
 #include "ray.h"
+#include "aabb.h"
 
 class material;
 
@@ -9,9 +10,11 @@ class hit_record {
 	public:
 		point3 p;
 		vec3 normal;
+		shared_ptr<material> mat;
 		double t;
 		bool front_face;
-		shared_ptr<material> mat;
+		double u;
+		double v;
 
 		void set_face_normal(const ray& r, const vec3& outward_normal) {
 			front_face = dot(r.direction(), outward_normal) < 0;
@@ -24,6 +27,8 @@ class hittable {
 		virtual ~hittable() = default;
 
 		virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+
+		virtual aabb bounding_box() const = 0;
 };
 
 #endif
